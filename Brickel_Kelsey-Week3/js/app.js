e22b4828b2959a56b4cfd132950c2b43644902ee
kmbrickel <kmbrickel@fullsail.edu>
2016-02-24 15:17:12 -0500

    Week 4 files
    Week 4 files

--- Brickel_Kelsey-Week3/js/app.js
+++ Brickel_Kelsey-Week3/js/app.js
@@ -1,3 +1,12 @@
 /**
  * Created by kelseybrickel on 2/20/16.
  */
+var myLists = anglar.module("MyApp",['ngRoute'])
+
+.config(function($routeProvider){
+
+    $routeProvider.when('/view1',{
+        templateUrl : "Quad1.html",
+        controller : ""
+    })
+})
